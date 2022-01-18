1d5e2f65eb08bf67e0ef074428eb081e46a09d8d
Egor <workdukend@gmail.com>
2022-01-18 13:14:55 +0300

    Add files via upload

---
+++ educational material/jupyter.c
@@ -0,0 +1,3 @@
+Как изменить рабочую директорию у Jupyter:
+https://shanyitan.medium.com/how-to-change-the-working-directory-of-jupyter-and-jupyter-lab-on-windows-environment-bbe5a5a99f05
+https://www.youtube.com/watch?v=K27iTAOwCbY
---
+++ educational material/Разное/как учиться программированию.c
@@ -0,0 +1,36 @@
+Marlin - Как правильно учиться программировать
+	https://www.youtube.com/watch?v=X7bNsCyiOm8&ab_channel=Marlin
+
+Marlin - План обучения для программиста	
+	https://www.youtube.com/watch?v=aCs42vWE8OE&ab_channel=Marlin
+
+Айти-Борода - Как стать программистом за неделю / Синдром самозванца / #itstart2019
+	https://www.youtube.com/watch?v=yd11QD72EvM&ab_channel=%D0%90%D0%B9%D0%A2%D0%B8%D0%91%D0%BE%D1%80%D0%BE%D0%B4%D0%B0
+	
++ См. "Как правильно задавать вопросы"
+
+
+Обзорное:
+	Айти-Борода - Как ШКОЛЬНИКУ Стать Программистом / Краткая Инструкция
+		https://www.youtube.com/watch?v=bdvrfwvGdWQ&ab_channel=%D0%90%D0%B9%D0%A2%D0%B8%D0%91%D0%BE%D1%80%D0%BE%D0%B4%D0%B0
+		00:59 - Что такое IT
+		02:13 - Как я попал в IT
+		07:55 - Возможности айтишника
+		13:08 - Кто есть в IT
+		20:35 - Как попасть в IT
+		26:57 - Полезные сайты
+		31:16 - Полезные youtube-каналы
+		
+	Sergey Nemchinskiy - Какие бывают направления в программировании?
+		https://www.youtube.com/watch?v=4XKrlOh3Iek&ab_channel=SergeyNemchinskiy
+
+		
+..Просто оставлю это здесь:
+
+	Канал TheCodingTrain
+		https://www.youtube.com/c/TheCodingTrain/videos
+		если серьезные каналы от "true-программистов" для вас слишком мрачные
+		
+	Что я хотел бы знать об IT в 20 лет. Максим Дорофеев
+		https://www.youtube.com/watch?v=fY8hhOv7lmE
+		О продуктивности, здоровье и соц. навыках в программировании
